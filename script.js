4d977243b3dad2a79fc7020d9fd5ff29938f970e
sairamdondapatii <sairamdondapatii@gmail.com>
2023-11-24 19:01:42 +0530

    updated

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function change(){
     
     if(mode.innerText=='dark_mode'){
         let body=document.getElementById('body')
-        body.style.backgroundColor='black'
+        body.style.backgroundColor='#292929'
         body.style.color='white'
         mode.innerText='light_mode'
         mode.style.color='white'
@@ -19,7 +19,7 @@ function change(){
         let icons=social.getElementsByClassName('social')
         
         for(let i=0; i<icons.length; i++){
-            icons[i].style.color='grey'
+            icons[i].style.color='white'
         }
     }
     else if(mode.innerText='light_mode'){
@@ -30,6 +30,8 @@ function change(){
         for(let i=0; i<links.length; i++){
             links[i].style.color='grey'
         }
+        let social=document.getElementById('social')
+        let icons=social.getElementsByClassName('social')
         for(let i=0; i<icons.length; i++){
             icons[i].style.color='black'
         }
